0796e70c8cb17c3a225be55161f6b2275d93574b
Samaira Sharma <samairasharma152@gmail.com>
2025-06-08 09:36:16 +0530

    caesarcipher.py

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,18 +1,18 @@
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-def caesar(orginal_text,shift_no,encode_decode):
+def caesar(original_text, shift_no, encode_decode):
     og_text = ""
-    for letter in orginal_text:
+    for letter in original_text:
         if letter not in alphabets:
             og_text += letter
-        if encode_decode == "decode":
-            shift_no *= -1
+        else:
+            if encode_decode == "decode":
+                shift_no *= -1
+            shifted_posn = alphabets.index(letter) + shift_no
+            shifted_posn %= len(alphabets)
+            og_text += alphabets[shifted_posn]
+    print(f"Here is the {encode_decode}: {og_text}")
 
-        shifted_posn = alphabets.index(letter) + shift_no
-        shifted_posn %= len(alphabets)
-        og_text += alphabets[shifted_posn]
-    print(f"Here is the {encode_decode} : {og_text}")
 should_continue = True
 while should_continue:
     direction = input("Type 'encode' to encrypt and 'decode' to decrypt:\n").lower()
