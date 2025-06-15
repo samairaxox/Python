b5bb46c6dbe5ee4253ff7d5e52d4f3e1c04c4a15
Samaira Sharma <samairasharma152@gmail.com>
2025-06-15 11:21:35 +0530

    PDF-Merger.py

--- PDF-Merger.py
+++ PDF-Merger.py
@@ -1,14 +1,14 @@
 from PyPDF2 import PdfMerger
 
 # Making a list of pdfs and a PdfMerger()
-pdfs = ["1.pdf", "2.pdf"]
-merger = PdfMerger()
+pdfs = ["1.pdf", "2.pdf"] # creates list of pdfs
+merger = PdfMerger() # initialises pdf merger
 
 # Adding pdfs in the PdfMerger()
 for pdf in pdfs:
     merger.append(pdf)
 
 # Creating a combined pdf
-merger.write("Combinedpdf.pdf")
+merger.write("Combinedpdf.pdf") # saves this with that name
 merger.close()
 
